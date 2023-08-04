539c4cca3544b28dd572340088e67356244f7f0e
eunbin00 <eun61n@gmail.com>
2023-08-04 16:56:41 +0900

    #1 - Feat: Add ThemeData

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lenfit/pages/home_screen.dart';
+import 'package:lenfit/pages/login_screen.dart';
 
 void main() => runApp(const LenFitApp());
 
@@ -8,8 +9,40 @@ class LenFitApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomeScreen(),
+    return MaterialApp(
+      title: 'Main',
+      theme: ThemeData(
+        textTheme: const TextTheme(
+            displayLarge: TextStyle(
+              color: Color(0xFF232B55),
+              fontSize: 32,
+            ),
+            bodyLarge: TextStyle(
+              color: Colors.white,
+              fontSize: 32,
+              fontWeight: FontWeight.w200,
+            ),
+            bodyMedium: TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+              fontWeight: FontWeight.w200,
+            ),
+            bodySmall: TextStyle(
+              color: Colors.white,
+              fontSize: 16,
+              fontWeight: FontWeight.w200,
+            )),
+        colorScheme: const ColorScheme.light().copyWith(
+          background: const Color(0x0011009e),
+        ),
+        primaryColor: const Color(0xFF11009E),
+      ),
+      // home: const HomeScreen(),
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => const LoginScreen(),
+        '/': (context) => const HomeScreen(),
+      },
     );
   }
 }
