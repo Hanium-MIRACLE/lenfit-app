b6d4639ec0949f228f7aeaf545e2f31c1c070ab7
eunbin00 <eun61n@gmail.com>
2023-08-06 13:31:50 +0900

    #1 - Edit TextTheme

---
+++ lib/utils/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+// Colors
+const cBackgroundColor = Color(0xFFD2FFF4);
+const cPrimaryColor = Color(0xFF000000);
+const cSecondaryColor = Color(0xFF0002A1);
+const cBlack = Color(0xFF000000);
